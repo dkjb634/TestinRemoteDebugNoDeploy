66c12575de6332c272678f890102f5e992ef4693
dkjb634 <dmitry.kazantsev@jetbrains.com>
2025-04-03 17:12:06 +0200

    test

---
+++ AppToDebug/Printer.cs
@@ -0,0 +1,9 @@
+﻿namespace AppToDebug;
+
+public class Printer
+{
+    public static void Print()
+    {
+        Console.WriteLine("Hello from AppToDebug");
+    }
+}
--- AppToDebug/Program.cs
+++ AppToDebug/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AppToDebug;
+
 Console.WriteLine("Hello, World!");
 
 string answer = Console.ReadLine();
+
+Printer.Print();
 
 Console.WriteLine(answer);
